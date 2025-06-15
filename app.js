99ed235736b7cbb8ac055853f6bef11f81757da3
nhthi <nguyenhungthinh574@gmail.com>
2025-06-15 17:47:33 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 
 const contactsRouter = require('./app/routes/contact.route');
+const ApiError = require('./app/api-error');
 
 app.use(cors());
 app.use(express.json());
@@ -13,5 +14,17 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/contacts', contactsRouter);
+
+// handle 404 responses
+app.use((req, res,next) => {
+  return next(new ApiError(404, `Resource not found`));
+});
+
+app.use((err, req, res, next) => {
+
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error',
+    }); 
+});
 
 module.exports = app;
